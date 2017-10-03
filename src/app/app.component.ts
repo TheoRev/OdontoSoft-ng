55ed0a5907877da6026787be184787ccb588fd2a
TheoRev <hrevillafernandez79@gmail.com>
2017-10-03 18:53:28 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PanelModule } from "primeng/primeng";
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PanelModule } from 'primeng/primeng';
+import { ButtonModule } from "primeng/primeng";
+import { SplitButtonModule } from "primeng/primeng";
+import { InputTextModule } from "primeng/primeng";
+import { PasswordModule } from "primeng/primeng";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    PanelModule,
+    ButtonModule,
+    SplitButtonModule,
+    InputTextModule,
+    PasswordModule
   ],
   providers: [],
   bootstrap: [AppComponent]
