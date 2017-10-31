6358c50a2e53921642b511123d5f66487d6db8b0
TheoRev <hrevillafernandez79@gmail.com>
2017-10-30 20:20:28 -0500

    Intentando consumir apirest

--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -6,27 +6,27 @@ import { Http, Headers } from "@angular/http";
 
 @Injectable()
 export class PatientService {
-  constructor(private http: Http) {}
+  constructor(private http: Http) { }
 
-  extractData:any;
-  handleErrorObservable:any;
+  extractData: any;
+  handleErrorObservable: any;
 
   public findAllPatients() {
     const endPoint = "http://localhost:3030/api/patients/";
 
     const headers = new Headers();
     // headers.append("Access-Control-Allow-Headers", "Content-Type");
-    
-    return this.http.get(endPoint, { headers: headers }).map(res => <Patient[]> res.json());
+
+    return this.http.get(endPoint, { headers: headers }).map(res => <Patient[]>res.json());
   }
-  
-  public createPatient(paciente){
+
+  public createPatient(paciente) {
     const endPoint = "http://localhost:3030/api/crud/patients/";
-    const headers = new Headers({ 'Content-Type': 'application/json' });
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Access-Control-Allow-Origin', '*');
     // let options = new RequestOptions({ headers: headers });
 
-    return this.http.post(endPoint, paciente, headers)
-                .map(this.extractData)
-               .catch(this.handleErrorObservable);
+    return this.http.post(endPoint, paciente, { headers: headers }).subscribe();
   }
 }
