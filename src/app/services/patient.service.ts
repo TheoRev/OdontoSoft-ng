b463d1e80b2bc82be039c12efa3343b969318d95
TheoRev <hrevillafernandez79@gmail.com>
2017-11-30 01:03:18 -0500

    Correccion de endpoint

--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -10,23 +10,24 @@ export class PatientService {
 
   extractData: any;
   handleErrorObservable: any;
+  endPoint: 'http://localhost:8000/api/v1/patients/';
 
   public findAllPatients() {
-    const endPoint = 'http://localhost:3030/api/patients/';
+    // const endPoint = 'http://localhost:3030/api/patients/';
 
     const headers = new Headers();
     // headers.append("Access-Control-Allow-Headers", "Content-Type");
 
-    return this.http.get(endPoint, { headers: headers }).map(res => <Patient[]>res.json());
+    return this.http.get(this.endPoint, { headers: headers }).map(res => <Patient[]>res.json());
   }
 
   public createPatient(paciente) {
-    const endPoint = 'http://localhost:3030/api/crud/patients/';
+    // const endPoint = 'http://localhost:3030/api/crud/patients/';
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Access-Control-Allow-Origin', '*');
     // let options = new RequestOptions({ headers: headers });
 
-    return this.http.post(endPoint, paciente, { headers: headers }).subscribe();
+    return this.http.post(this.endPoint, paciente, { headers: headers }).subscribe();
   }
 }
